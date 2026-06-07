c6ef3f8d4d5d63b96dae0d4371181f0bf668866f
Raghuveer842958 <raghuveerchauhan7787@gmail.com>
2026-06-08 01:20:52 +0530

    fixed document retrival issue

--- ai/rag/retriever.py
+++ ai/rag/retriever.py
@@ -1,7 +1,11 @@
-from ai.rag.vectorstore import retriever
+# from ai.vectorstore import get_retriever
+
+from ai.rag.vectorstore import get_retriever
+
 
 def retrieve_resume_context(query):
 
+    retriever = get_retriever()
     retrieved_docs = retriever.invoke(query)
 
     print("\nRetrieved Chunks:\n")
--- ai/rag/vectorstore.py
+++ ai/rag/vectorstore.py
@@ -1,16 +1,44 @@
+# from langchain_community.vectorstores import FAISS
+
+# from ai.factories.embedding_factory import get_embeddings
+# from ai.config.config import VECTOR_DB_PATH
+
+# embeddings = get_embeddings()
+
+# vectorstore = FAISS.load_local(
+#     VECTOR_DB_PATH,
+#     embeddings,
+#     allow_dangerous_deserialization=True
+# )
+
+# retriever = vectorstore.as_retriever(
+#     search_kwargs={"k": 3}
+# )
+
+
+
 from langchain_community.vectorstores import FAISS
 
 from ai.factories.embedding_factory import get_embeddings
 from ai.config.config import VECTOR_DB_PATH
 
-embeddings = get_embeddings()
+_retriever = None
 
-vectorstore = FAISS.load_local(
-    VECTOR_DB_PATH,
-    embeddings,
-    allow_dangerous_deserialization=True
-)
 
-retriever = vectorstore.as_retriever(
-    search_kwargs={"k": 3}
-)
+def get_retriever():
+    global _retriever
+
+    if _retriever is None:
+        embeddings = get_embeddings()
+
+        vectorstore = FAISS.load_local(
+            VECTOR_DB_PATH,
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
+
+        _retriever = vectorstore.as_retriever(
+            search_kwargs={"k": 3}
+        )
+
+    return _retriever
